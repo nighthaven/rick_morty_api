360f3426ee5db7ce769829a013fc895dde500f36
Boris Le Bon <ooborislebonoo@gmail.com>
2022-07-09 00:03:30 +0200

    refactor: :recycle: refactor export to csv part

--- export/export.py
+++ export/export.py
@@ -1,28 +1,29 @@
+
 from importlib.resources import path
 from fastapi import APIRouter, Depends
-from database.database import get_db
+import sqlalchemy
+from database.database import get_db, engine
 from database import to_db
 from sqlalchemy.orm import Session
-import pandas
+import csv
 
 
 path=APIRouter()
 
 @path.get("/export")
-def export_comment(db: Session = Depends(get_db)):
-    query_comments=db.query(to_db.Comments).all()
-    return query_comments
-
-"""    comment_id = [data[0] for data in data_comments]
-    user_id = [data[1] for data in data_comments]
-    character_id = [data[2] for data in data_comments]
-    episode_id = [data[3] for data in data_comments]
-    message = [data[4] for data in data_comments]
-    data_to_transform = {"comment_id": comment_id, "user_id": user_id, "character_id": character_id, "episode_id": episode_id, "message": message}
-    data_to_pandas = pandas.DataFrame(data_to_transform)
-    return data_to_pandas.to_csv("export/comment.csv")"""
-
-    
+def export(db: Session = Depends(get_db)):
+    metadata = sqlalchemy.MetaData()
+    metadata.bind = engine
+    mytable = sqlalchemy.Table('comments', metadata, autoload=True)
+    db_connection = engine.connect()
+    select = sqlalchemy.sql.select([mytable])
+    result = db_connection.execute(select)
+    fh = open('export/comment_export.csv', 'w')
+    outcsv = csv.writer(fh)
+    outcsv.writerow(result.keys())
+    outcsv.writerows(result)
+    fh.close
+    return "export done"
     
 
 
